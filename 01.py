8b1f5f8717c0098947e93e861f934f2994681c58
genie <60090391+blingblin-g@users.noreply.github.com>
2020-10-20 00:53:59 +0900

    Create 01.py

---
+++ 01.py
@@ -0,0 +1,40 @@
+"""
+As you can see, the code is broken.
+Create the missing functions, use default arguments.
+Sometimes you have to use 'return' and sometimes you dont.
+Start by creating the functions
+"""
+
+def is_on_list(days, day):
+  return day in days
+
+def get_x(days, number):
+  number = int(number)
+  return days[number]
+
+def add_x(days, day):
+  return days.append(day)
+
+def remove_x(days, day):
+  return days.remove(day)
+
+# \/\/\/\/\/\/\  DO NOT TOUCH AREA  \/\/\/\/\/\/\ #
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+print("Is Wed on 'days' list?", is_on_list(days, "Wed"))
+
+print("The fourth item in 'days' is:", get_x(days, 3))
+
+add_x(days, "Sat")
+print(days)
+
+remove_x(days, "Mon")
+print(days)
+
+
+# /\/\/\/\/\/\/\ END DO NOT TOUCH AREA /\/\/\/\/\/\/\ #
+
+
+
+
